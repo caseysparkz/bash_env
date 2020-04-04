a792fa9ce84451a7b16f89de11c8d00b4dc1d7ab
transnat <caseyalexsparks@gmail.com>
2020-04-04 12:15:50 -0700

    Fixed arch.sh install script.

--- distro_packages/arch.sh
+++ distro_packages/arch.sh
@@ -1,23 +1,23 @@
 #! /bin/bash
 sudo sh -c "
 	pacman -Syu;
-	pacman -S
-		facter		\
-		libnotify-bin	\
-		lm_sensors	\
-		nmap		\
-		ntp		\
-		python		\
-		python-dev	\
-		python-pip	\
-		python-venv	\
-		sl		\
-		snapd		\
-		speedtest-cli	\
-		thefuck		\
-		tldr		\
+	pacman -S			\
+		facter			\
+		libnotify		\
+		lm_sensors		\
+		nmap			\
+		ntp			\
+		python			\
+		python-pip		\
+		python-virtualenv	\
+		sl			\
+		snapd			\
+		speedtest-cli		\
+		thefuck			\
+		tldr			\
 		vim
 "
+
 if [ ! -L /snap ]; then
 	sudo ln -s /var/lib/snapd/snap /snap
 fi
