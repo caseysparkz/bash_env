7eae1c48be27ad2c56ff7e7d43f16663c427c673
transnat <caseyalexsparks@gmail.com>
2020-04-19 14:26:59 -0700

    Added, then deleted, Go to/from installation packages.

--- distro_packages/debian.sh
+++ distro_packages/debian.sh
@@ -3,20 +3,20 @@ sudo sh -c "
 	apt update;
 	apt upgrade -y;
 	apt install -y	\
-		facter		    \
-		libnotify-bin	\
-		lm-sensors	  \
-		nmap		      \
-		ntp		        \
-		python3		    \
-		python3-dev	  \
-		python3-pip	  \
-		python3-venv	\
-		safe-rm		    \
-		sl		        \
-		snapd		      \
-		speedtest-cli	\
-		thefuck		    \
-		tldr		      \
+		facter \
+		libnotify-bin \
+		lm-sensors \
+		nmap \
+		ntp \
+		python3 \
+		python3-dev \
+		python3-pip \
+		python3-venv \
+		safe-rm \
+		sl \
+		snapd \
+		speedtest-cli \
+		thefuck \
+		tldr \
 		vim
 "
--- distro_packages/rhel.sh
+++ distro_packages/rhel.sh
@@ -2,21 +2,21 @@
 sudo sh -c "
 	yum update;
 	yum upgrade -y;
-	yum install -y	\
-		facter		    \
-		libnotify-bin	\
-		lm-sensors	  \
-		nmap		      \
-		ntp		        \
-		python3		    \
-		python3-dev	  \
-		python3-pip	  \
-		python3-venv	\
-		safe-rm		    \
-		sl		        \
-		snapd		      \
-		speedtest-cli	\
-		thefuck		    \
-		tldr		      \
+	yum install -y \
+		facter \
+		libnotify-bin \
+		lm-sensors \
+		nmap \
+		ntp \
+		python3 \
+		python3-dev \
+		python3-pip \
+		python3-venv \
+		safe-rm \
+		sl \
+		snapd \
+		speedtest-cli \
+		thefuck \
+		tldr \
 		vim
 "
