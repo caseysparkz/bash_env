c4291abf66c92818a370cd17bb8354e65e39f921
caseysparkz <himself@caseyspar.kz>
2020-09-12 19:52:48 -0700

    Removed unneeded variable.

--- vars.sh
+++ vars.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+# User defaults
 export EDITOR=vim
 export TERM=xterm-256color
 export SCRIPTS="${HOME}/.env/scripts"
 export FUNCTIONS="${HOME}/.env/functions"
 export PATH="${PATH}:${SCRIPTS}"
-export PLAY="/etc/ansible/inventory"
 
+# Yubikey+SSH variables for .bashrc
 export GPG_TTY="$(tty)"
 export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
 
