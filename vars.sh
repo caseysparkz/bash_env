a2fa75a653fffe8c1cbdc081399612682de42805
caseysparkz <himself@caseyspar.kz>
2020-11-12 21:23:27 -0800

    Deleted some useless functions. Added gsh.

--- vars.sh
+++ vars.sh
@@ -5,6 +5,7 @@ export EDITOR=vim
 export TERM=xterm-256color
 export FUNCTIONS="${HOME}/.env/functions"
 export SCRIPTS="${HOME}/.env/scripts"
+export GSH="${HOME}/.env/gsh"
 
 # Yubikey+SSH variables for .bashrc
 export GPG_TTY="$(tty)"
