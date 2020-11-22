70496966e7ebac306ce54fe77f5e70217ee5a305
caseysparkz <himself@caseyspar.kz>
2020-11-21 23:27:31 -0800

    Minor edits.

--- scripts/gshmake.sh
+++ scripts/gshmake.sh
@@ -6,11 +6,14 @@
 # Remove shebang from cleartext.
 sed -i '/\#\!\/bin\//d' $1
 
+# Remove empty lines from cleartext.
+sed -i '/^$/d' $1
+
 # Encrypt original script and output to a temporary file.
 gpg2 --output /tmp/$1.asc --sign --armor --encrypt $1 
 
 # Create GSH script and prepend commands to interpret encrypted portion.
-cat > ${GSH}/$1 << EOF
+cat > $1.gsh << EOF
 #!/bin/bash
 
 gpg2 --decrypt 2> /dev/null << EOF | bash -s \$@
@@ -18,14 +21,15 @@ gpg2 --decrypt 2> /dev/null << EOF | bash -s \$@
 EOF
 
 # Import encrypted portion from the temporary file.
-cat /tmp/$1.asc >> ${GSH}/$1
+cat /tmp/$1.asc >> $1.gsh
 
 # Append EOF to close the PGP-encrypted portion.
-printf "\nEOF" >> ${GSH}/$1
+printf "\nEOF" >> $1.gsh
 
-# Remove temporary file
+# Remove temporary file and original.
 rm /tmp/$1.asc
 
 # Change GSH script owner to user and make executable.
-chown $(whoami):$(whoami) ${GSH}/$1
-chmod +x ${GSH}/$1
+chown $(whoami):$(whoami) $1.gsh
+chmod +x $1.gsh
+
