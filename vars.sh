730d3c3dceb8f83697b65e9e3fa3983f723d3315
Casey Sparks <himself@caseyspar.kz>
2020-11-23 14:30:13 -0800

    Minor formatting.

--- vars.sh
+++ vars.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# User defaults
+# Personal defaults.
 export EDITOR=vim
 export TERM=xterm-256color
 export FUNCTIONS="${HOME}/.env/functions"
 export SCRIPTS="${HOME}/.env/scripts"
 export GSH="${HOME}/.env/gsh"
 
-# Yubikey+SSH variables for .bashrc
+# GPG variables for SSH.
 export GPG_TTY="$(tty)"
 export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-
