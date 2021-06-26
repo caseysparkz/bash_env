d50bbb6af019ed2eac19302c9e09b18cc2f617a0
caseysparkz <github@caseyspar.kz>
2021-06-25 17:00:44 -0700

    Changing vim-gnupg to submodule.

--- install.sh
+++ install.sh
@@ -1,38 +1,31 @@
 #!/bin/env bash
 
 ### Change ownership to user and hide directory.
-chown -R $(whoami) ${HOME}/env
+chown -R $(whoami):$(whoami) ${HOME}/env
 mv ${HOME}/env ${HOME}/.env
 
 ### Remove existing profile and symlink new profile.
-if [ -f ${HOME}/.profile ] && [ ! -L ${HOME}/.profile ]; then
-  rm ${HOME}/.profile
-fi
+rm ${HOME}/.profile
 ln -s ${HOME}/.env/profile ${HOME}/.profile
 
 ### Remove existing bashrc and symlink new bashrc.
-if [ -f ${HOME}/.bashrc ] && [ ! -L ${HOME}/.bashrc ]; then
-  rm ${HOME}/.bashrc
-fi
+rm ${HOME}/.bashrc
 ln -s ${HOME}/.env/bashrc ${HOME}/.bashrc
 
-
 ### Remove existing screenrc and symlink to new bashrc.
-if [ -f ${HOME}/.screenrc ] && [ ! -L ${HOME}/.screenrc ]; then
-  rm ${HOME}/.screenrc
-fi
+rm ${HOME}/.screenrc
 ln -s ${HOME}/.env/screen/screenrc ${HOME}/.screenrc
 
 ### Remove vim settings and symlink to new settings.
-if [ -d ${HOME}/.vim ] && [ ! -L ${HOME}/.vim ]; then
-  rm -rf ${HOME}/.vim
-fi
+rm -rf ${HOME}/.vim
 ln -s ${HOME}/.env/vim ${HOME}/.vim
 
-if [ -f ${HOME}/.vimrc ] && [ ! -L ${HOME}/.vimrc ]; then
-  rm ${HOME}/.vimrc
-fi
+rm ${HOME}/.vimrc
 ln -s ${HOME}/.env/vim/vimrc ${HOME}/.vimrc
+
+### Remove git config and symlink to new settings.
+rm ${HOME}/.gitconfig
+ln -s ${HOME}/.env/git/gitconfig ${HOME}/.gitconfig
 
 ### Load new profile
 source ${HOME}/.profile
